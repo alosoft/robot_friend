c6d2e497384ae59ef039b94f2b6f8d34181c9261
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-10-22 17:21:02 +0000

    end of lesson 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {robots} from "./robots";
+// import {robots} from "./robots";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import './App.css'
@@ -13,9 +13,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+    // console.log('constructor')
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+    // console.log('componentDidMount')
   }
 
   // this function tracks event in search box
@@ -30,13 +38,18 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
-    return (
-        <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots}/>
-        </div>
-    )
+    // console.log('render');
+    if (this.state.robots === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+          <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+          </div>
+      )
+    }
   }
 }
 
